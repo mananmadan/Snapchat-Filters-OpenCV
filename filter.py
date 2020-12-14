ade2d19a79ba269cd540a93cb3a17cde517fd39d
mananmadan <madan.manan1@gmail.com>
2020-12-14 21:13:09 +0530

    debugging

--- filter.py
+++ filter.py
@@ -1,44 +1,13 @@
-import numpy as np
-import cv2 
-import matplotlib.pyplot as plt
+import cv2
 
-## load the filters
+# load the filters
 glasses = cv2.imread('filters/glasses-2.jpg')
 moustache = cv2.imread('filters/moustache-2.jpg')
 
-## dictionary for the landmarks
+# dictionary for the landmarks
 mydict = {}
-name_list = [
-'left_eye_center_x',
-'left_eye_center_y',
-'right_eye_center_x',
-'right_eye_center_y',
-'left_eye_inner_corner_x',
-'left_eye_inner_corner_y',
-'left_eye_outer_corner_x',
-'left_eye_outer_corner_y',
-'right_eye_inner_corner_x',
-'right_eye_inner_corner_y',
-'right_eye_outer_corner_x',
-'right_eye_outer_corner_y',
-'left_eyebrow_inner_end_x',
-'left_eyebrow_inner_end_y',
-'left_eyebrow_outer_end_x',
-'left_eyebrow_outer_end_y',
-'right_eyebrow_inner_end_x',
-'right_eyebrow_inner_end_y',
-'right_eyebrow_outer_end_x',
-'right_eyebrow_outer_end_y',
-'nose_tip_x',
-'nose_tip_y',
-'mouth_left_corner_x',
-'mouth_left_corner_y',
-'mouth_right_corner_x',
-'mouth_right_corner_y',
-'mouth_center_top_lip_x',
-'mouth_center_top_lip_y',
-'mouth_center_bottom_lip_x',
-'mouth_center_bottom_lip_y']
+name_list = ['left_eye_center_x', 'left_eye_center_y', 'right_eye_center_x', 'right_eye_center_y', 'left_eye_inner_corner_x', 'left_eye_inner_corner_y', 'left_eye_outer_corner_x', 'left_eye_outer_corner_y', 'right_eye_inner_corner_x', 'right_eye_inner_corner_y', 'right_eye_outer_corner_x', 'right_eye_outer_corner_y', 'left_eyebrow_inner_end_x', 'left_eyebrow_inner_end_y', 'left_eyebrow_outer_end_x', 'left_eyebrow_outer_end_y', 'right_eyebrow_inner_end_x', 'right_eyebrow_inner_end_y', 'right_eyebrow_outer_end_x', 'right_eyebrow_outer_end_y', 'nose_tip_x', 'nose_tip_y', 'mouth_left_corner_x', 'mouth_left_corner_y', 'mouth_right_corner_x', 'mouth_right_corner_y',
+'mouth_center_top_lip_x', 'mouth_center_top_lip_y', 'mouth_center_bottom_lip_x','mouth_center_bottom_lip_y']
 
 ## load the dict
 def make_dict(label_points):
